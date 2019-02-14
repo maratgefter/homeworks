5325d8a7332ef48aab054ff2b090182569a1d670
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-02-14 21:09:44 +0300

    update

--- 13022019/index.php
+++ 13022019/index.php
@@ -10,9 +10,10 @@
 </head>
 <body>
     <form action="" method="post">
-        <textarea name="feld" id="feld" cols="30" rows="10"></textarea>
-        <input type="submit" value="Давай!">
-    </form>
+        <textarea name="feld" id="feld" cols="30" rows="10" placeholder="<?=(isset($_POST["feld"])) ? $_POST["feld"] : "Введите число";?>"></textarea><br><br>
+        <input type="number" name="num" placeholder="<?=(isset($_POST["num"])) ? $_POST["num"] : "Введите число";?>"><br><br>
+        <input type="submit" value="Давай!"><br><br>
+        </form>
 </body>
 </html>
 
@@ -21,10 +22,15 @@
 //echo "Число символов в тексте: ".preg_match_all('/\w{1}/u', $_POST["feld"], $mathes)."!";
 echo "Число символов в тексте: ".iconv_strlen($_POST["feld"])."!";
 echo "<br><br>";
-echo "Число слов в тексте: ".preg_match_all('/\b\w{1,15}\b/u', $_POST["feld"], $mathes)."!";
+echo "Число слов в тексте: ".preg_match_all('/\b\w{1,15}\b/u', $_POST["feld"], $matches)."!";
 echo "<br><br>";
-echo "Число слов с 10-ю символами: ".preg_match_all('/\b\w{10}\b/u', $_POST["feld"], $mathes)."!";
+echo "Число слов с ".$_POST["num"]." символами: ".preg_match_all('/\b\w{'.$_POST["num"].'}\b/u', $_POST["feld"], $mathes)."!";
 echo "<br><br>";
-print_r ($mathes);
+//print_r ($mathes);
+echo "<ol>";
+foreach ($matches[0] as $value) {
+    echo '<li>'.$value.'</li>';
+}
+echo "</ol>";
 ?>
 </pre>
