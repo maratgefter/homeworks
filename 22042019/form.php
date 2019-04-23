9241a53cdd2fc15c54bef42e346e5e24c4c823d2
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-04-23 21:11:17 +0300

    changes

--- 22042019/form.php
+++ 22042019/form.php
@@ -1,47 +1,54 @@
 <?
 
-class Form {
-    protected $name;
-    protected $text;
-    protected $attrName = "";
-    protected $attrValue = "";
+class Form 
+{
+    protected $action;
+    protected $method;
+    protected $inputTag = "";
 
-    function __construct ($name, $attrName, $attrValue, $text = null) {
-        $this->name = $name;
-        $this->text = $text;
-        $this->attrName = $attrName;
-        $this->attrValue = $attrValue;
+    function __construct($method, $action)
+    {
+        $this->action = $action;
+        $this->method = $method;
     }
 
-    function show_form () {
-        $str = "";
-        if ($this->attrName != "" && $this->attrValue != "") {
-            if (is_array($this->attrName) && is_array($this->attrValue)) {
-                foreach (array_combine($this->attrName, $this->attrValue) as $v => $v2) {
-                    $str .= " $v=\"$v2\"";
-                }
-            } else {
-                $str = " $this->attrName=\"$this->attrValue\"";
-            }
-        }
-        if ($this->name == "textarea" || $this->name == "button") {
-            return "<$this->name$str>$this->text</$this->name>";
-        } else if ($this->name == "input") {
-            return "<$this->name$str>";
-        }
+    function add_input($type, $name='', $value='')
+    {
+        $addName = "";
+        if($name != '' ) {
+            $addName = " name='$name'";
+        } 
+
+        $addvalue = "";
+        if($value != '' ) {
+            $addvalue = " value='$value'";
+        } 
+
+        $this->inputTag .= "\t<input type='$type'$addName$addvalue>\n";
+    }
+
+    protected function coupleTag ($name, $attr="", $text="") 
+    {
+        $this->inputTag .= "\t<$name". $attr=($attr != '') ? $attr : ''.">$text</$name>\n";
+    }
+
+    function addTextarea($attr="", $text="")
+    {
+        return  $this->coupleTag('textarea', $attr="", $text="");
+    }
+
+    function show_form()
+    {
+        return "<form method=\"$this->method\" action=\"$this->action\">\n$this->inputTag</form>";
     }
 }
 
-$obj = new Form ('input', 'type', 'text');
-$obj2 = new Form ('textarea', ['name', 'cols', 'rows'], ['text_example', '50', '25'], 'текстовая область');
-$obj3 = new Form ('input', ['type', 'name', 'placeholder'], ['password', 'password', 'Введите ваш пароль']);
-$obj4 = new Form ('button', 'name', 'button', 'Ok');
+$obj = new Form('post', 'index.php');
+$obj->add_input('text', 'login', '');
+$obj->add_input('password', 'password', '');
+$obj->add_input('submit', '', 'Send');
+$obj->addTextarea('', '');
+//$obj->coupleTag('button', '', 'Take');
 
-echo "<form>";
-echo $obj -> show_form ();
-echo $obj2 -> show_form ();
-echo $obj3 -> show_form ();
-echo $obj4 -> show_form ();
-echo "<form>";
-
+echo $obj->show_form();
 ?>
