3c84b2c6aba4a677088e187922ce697d9e23c8bc
maratgefter <47002971+maratgefter@users.noreply.github.com>
2019-04-30 21:13:11 +0300

    update

--- 29042019/table.php
+++ 29042019/table.php
@@ -10,20 +10,85 @@
 
         function showTable()
         {
-            echo '<table>';
+            echo "<table>\n";
             foreach($this->table as $v) {
-                echo '<tr>';
+                echo "\t<tr>\n";
                 foreach($v as $val) {
-                    echo "<td>$val</td>";                    
+                    echo "\t\t<td>$val</td>\n";                    
                 }
-                echo '</tr>';
+                echo "\t</tr>\n";
             }
-            echo "</table>";
+            echo "</table><hr>";
         }
 
+        function add_row ($var, $key)
+        {
+            $new_array = [];
+            foreach ($this->table as $k => $v) {
+                if ($key == $k) {
+                    $new_array[] = $var;
+                }
+                $new_array[] = $v;
+            }
+            $this->table = $new_array;
+        }
+
+        function deleteRow ($k)
+        {
+            unset($this->table[$k]);
+        }
+
+        function sortArray ($num)
+        {
+            $new_array = [];
+            foreach ($this->table as $k=>$v) {
+                $new_array[$k] = $v[$num];
+            }
+            array_multisort($new_array, $this->table);
+        }
+
+        function deleteCol ($key)
+        {
+            foreach ($this->table as$k=>$v) {
+                unset ($this->table[$k][$key]);
+            }
+        }
+
+        function test (&$table, $var, $key)
+        {
+            $buf_array = [];
+            foreach ($table as $k => $v) {
+                if ($key == $k) {
+                    $buf_array[] = $var;
+                }
+                $buf_array = $v;
+            }
+            $table = $buf_array;
+        }
+
+        function addCol ($table, $key)
+        {
+            foreach ($this->table as $k => $v) {
+                $this->test($this->table[$k], $table[$k], $key);
+            }
+            return $this->array;
+        }
+
+    }
+    
     $obj = new Table(
     [['№', 'Вид работ', 'Ответственный', 'Должность'], 
     ['1', 'Уборка территории', 'Иванов', 'Инженер-механик'],
     ['2', 'Мытье окон', 'Сидоров', 'Инженер-электрик']]);
+    echo $obj->showTable();
+    $obj->add_row(['4', 'Вынос мусора', 'Поликарпов', 'Инженер-энергетик'], 1);
+    echo $obj->showTable();
+    $obj->deleteRow(1);
+    echo $obj->showTable();
+    $obj->sortArray(0);
+    echo $obj->showTable();
+    $obj->deleteCol(3);
+    echo $obj->showTable();
+    print_r ($obj -> addCol(['Стаж работы', 2, 9], 2));
     echo $obj->showTable();
 ?>
